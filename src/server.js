4856630c128a3355f8f9e33508616293aecbc88c
Who <ehreh789@naver.com>
2021-09-26 20:39:44 +0900

    20210926

--- src/server.js
+++ src/server.js
@@ -4,17 +4,32 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) =>{
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if(url === "/protected") {
+        return res.send("<h1>Not Allowed</h1>")
+    }
+    console.log("Allowed, you may continue");
+    next();
+};
+
 const handelHome = (req, res) => {
     return res.send("<h1>I still love you.</h1>");
-}
+};
 
-const handleLogin = (req, res) =>{
-    return res.send({ message: "Login here." });
-}
+const handleProtected = (req, res) => {
+    return res.send("Welcome to the private lounge");
+};
 
+app.use(logger);
+app.use(privateMiddleware);
 app.get("/", handelHome);
-app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
-app.listen(4000, handleListening);
+app.listen(PORT, handleListening);
