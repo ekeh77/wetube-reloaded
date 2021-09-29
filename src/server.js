e6060a54357855531569471149ef9c9099b486d7
Who <ehreh789@naver.com>
2021-09-29 21:48:59 +0900

    20210929

--- src/server.js
+++ src/server.js
@@ -1,21 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
-
-const logger = (req, res, next) =>{
-    console.log(`${req.method} ${req.url}`);
-    next();
-};
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if(url === "/protected") {
-        return res.send("<h1>Not Allowed</h1>")
-    }
-    console.log("Allowed, you may continue");
-    next();
-};
+const logger = morgan("dev")
 
 const handelHome = (req, res) => {
     return res.send("<h1>I still love you.</h1>");
@@ -25,8 +14,7 @@ const handleProtected = (req, res) => {
     return res.send("Welcome to the private lounge");
 };
 
-app.use(logger);
-app.use(privateMiddleware);
+app.use(logger("dev"));
 app.get("/", handelHome);
 app.get("/protected", handleProtected);
 
