8842141577a0b182669ca56a08421a8866a124ba
Who <ehreh789@naver.com>
2021-10-02 16:47:51 +0900

    20211002

--- src/server.js
+++ src/server.js
@@ -1,22 +1,18 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
 const app = express();
-const logger = morgan("dev")
+const logger = morgan("dev");
+app.use(logger);
 
-const handelHome = (req, res) => {
-    return res.send("<h1>I still love you.</h1>");
-};
-
-const handleProtected = (req, res) => {
-    return res.send("Welcome to the private lounge");
-};
-
-app.use(logger("dev"));
-app.get("/", handelHome);
-app.get("/protected", handleProtected);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
