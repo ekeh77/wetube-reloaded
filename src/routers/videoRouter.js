5c099dc98d82e07eb93c299b225bac3dae9e681c
Who <ehreh789@naver.com>
2021-10-28 21:17:59 +0900

    20211028

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,12 +4,15 @@ import {
     getEdit, 
     upload, 
     deleteVideo,
-    postEdit
+    postEdit,
+    getUpload,
+    postUpload
 } from "../controllers/videoController"
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
