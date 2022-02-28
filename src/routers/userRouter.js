14d8806e1e9be6b0e50169af9b7c965c636b84c4
Who <ehreh789@naver.com>
2022-02-28 19:48:45 +0900

    20220228

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -6,7 +6,9 @@ import {
     logout, 
     see, 
     startGithubLogin,
-    finishGithubLogin
+    finishGithubLogin,
+    getChangePassword,
+    postChangePassword
 }from "../controllers/userController"
 import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
@@ -14,9 +16,14 @@ const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
 userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
-userRouter.get("/remove", remove);
+userRouter
+    .route("/change-password")
+    .all(protectorMiddleware)
+    .get(getChangePassword)
+    .post(postChangePassword);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
+
 userRouter.get("/:id", see);
 
 export default userRouter; 
