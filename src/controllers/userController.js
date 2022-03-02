14870f5ed803ad3e315dba969110520727195fcc
Who <ehreh789@naver.com>
2022-03-02 20:18:21 +0900

    20220302

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -142,6 +142,7 @@ export const postEdit = async (req, res) => {
             user: { _id }  
         },
         body: { name, email, username, location },
+        file,
     } = req;
     const updatedUser = await User.findByIdAndUpdate(
         _id, {
