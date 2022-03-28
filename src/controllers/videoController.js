2fe5e00aebedfdee4f03a4c81a6003988786dc4e
Who <ehreh789@naver.com>
2022-03-28 20:39:46 +0900

    20220328

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,7 +5,7 @@ import User from "../models/Video";
   });
 */
 export const home = async(req, res) => {
-    const videos = await Video.find({})
+    const videos = await Video.find({}).sort({ createdAt: "desc"})
     .sort({ createdAt:"desc" })
     .populate("owner");
     return res.render("home", { pageTitle: "Home", videos })
