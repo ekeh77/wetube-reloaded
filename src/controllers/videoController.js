e5f8bfd17b8d0a60d2565588367043a958395d6b
Who <ehreh789@naver.com>
2022-04-15 19:22:42 +0900

    20220415

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -121,10 +121,10 @@ export const registerView = async(req, res) => {
   const {id} = req.params;
   const video = await Video.findById(id);
   if(!video){
-    return res.status(404);
+    return res.SendStatus(404);
   }
   video.meta.views = video.meta.views + 1;
   await video.save();
-  return res.status(200);
+  return res.SendStatus(200);
 
 }
